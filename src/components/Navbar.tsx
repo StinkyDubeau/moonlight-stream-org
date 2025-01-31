9200941012d063c2394aa652aac9723c3646d368
Jake Dubeau <jakersunderscore09@gmail.com>
2025-01-31 15:25:30 -0500

    Configure github pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return (
-        <ul className="flex gap-2">
+        <ul className="flex w-screen gap-2 bg-slate-800 text-slate-100">
             <li>GitHub</li>
         </ul>
     );

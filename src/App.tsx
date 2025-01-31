04c8e2ed69f79e9ad5ae72ce02b3cde8eb1ea50c
Jake Dubeau <jakersunderscore09@gmail.com>
2025-01-31 15:05:53 -0500

    Remove vite branding, enforce prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-function App() {
-  return (
-    <>
-      <div className="flex flex-col justify-center h-screen">
-        <p className="m-auto p-2 shadow rounded bg-slate-800 text-slate-100">Testing tailwind!</p>
-      </div>
-    </>
-  );
-}
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
-export default App;
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            <Home />
+        </>
+    );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <ul className="flex gap-2">
+            <li>GitHub</li>
+        </ul>
+    );
+}
